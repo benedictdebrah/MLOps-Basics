ef7c08120bca89fa43e283e05cf86120a3d50e7a
benedictdebrah <benedictdebrah.o@gmail.com>
2024-04-27 20:49:08 +0000

    to either run on local or  dagshub

--- README.md
+++ README.md
@@ -21,13 +21,31 @@ Here are some of the MLflow features I've explored in this project:
 
 To run this project, you need to have Python and MLflow installed on your machine. Once you have these prerequisites, you can follow these steps:
 
-1. Clone the repository to your local machine.
+1. Clone the repository to your local machine (install requirements).
 2. Navigate to the project directory in the terminal.
 3. Run the main script with the command: `python main.py`
 4. After running the script, start the MLflow tracking server with the command: `mlflow ui`
-5. Open a web browser and go to `http://localhost:5000` to view the MLflow UI and see the results of your script.
+5. After running `mlflow ui` the link that appears on the terminal click on it to access the platform
+
+
+
+## Running the Project on DagsHub
+
+To run this project on DagsHub, you need to have a DagsHub account. Once you have an account, you can follow these steps:
+
+1. Fork the repository to your DagsHub account.
+2. Set up the environment variables for MLflow tracking. You can do this by running the following commands in your terminal:
+
+```bash
+export MLFLOW_TRACKING_URI=https://dagshub.com/your_username/your_repo_name.mlflow
+export MLFLOW_TRACKING_USERNAME=your_username
+export MLFLOW_TRACKING_PASSWORD=your_password
+```
+3. Run the script `python main.py`
+
 
 Please note that this project is a basic introduction to MLflow and does not follow a rigorous data science workflow.
+
 ## Future Work
 
 I plan to explore more advanced features of MLflow and use it in more complex machine learning projects. I also plan to follow a more rigorous data science workflow in my future projects.
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import datasets
 import mlflow
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
+import os
 
 import mlflow.sklearn
 
@@ -29,6 +29,15 @@ def evaluate_metrics(actual, predicted):
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(40)
+
+    #One must be commented **either** the local or the remote tracking URI
+    
+    # Set the local directory to log MLflow runs(uncomment the line below if you want to run on local)
+    # mlflow.set_tracking_uri('file://' + os.path.abspath('./mlruns'))
+
+    #Set the tracking URI based on the environment (uncomment this codes if you want to run on dagshub)
+    remote_server_uri = "https://dagshub.com/bende.tymer/MLOps-Basics.mlflow"
+    mlflow.set_tracking_uri(remote_server_uri)
 
     # split the dataset
     train, test = train_test_split(df, test_size=0.2)
@@ -52,15 +61,6 @@ if __name__ == "__main__":
     recall = recall_score(test_y, predicted_, average='weighted')
     f1 = f1_score(test_y, predicted_, average='weighted')
 
-    # Set the tracking URI based on the environment
-    if "dagshub.com" in urlparse(mlflow.get_tracking_uri()).netloc:
-        # For Dagshub
-        remote_server_uri = "https://dagshub.com/bende.tymer/MLOps-Basics.mlflow"
-        mlflow.set_tracking_uri(remote_server_uri)
-    else:
-        # For local
-        mlflow.set_tracking_uri("file:/home/benedictdebrah/Desktop/MLOps-Basics/mlruns")
-
     # Log mlflow attributes for mlflow UI
     metrics = {
         "accuracy": accuracy,
@@ -71,7 +71,6 @@ if __name__ == "__main__":
 
     mlflow.log_metrics(metrics)
     mlflow.sklearn.log_model(rc, "model")
-
 
 
 
